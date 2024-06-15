6f6512bff0368840dd204e3f9646b88cd88329b5
Michal <97395447+hardrave@users.noreply.github.com>
2024-06-15 18:09:10 +0200

    vision integration

---
+++ main.py
@@ -0,0 +1,46 @@
+import base64
+import requests
+import os
+import json
+
+api_key  = os.getenv("APIKEY")
+
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+image_path = "/home/mich/smieci/3.jpg"
+
+base64_image = encode_image(image_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4o",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "You are an ai recycling bin. You gives one word replying, classifying the image as plastic, paper or glass."
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+
+print(response.json())
