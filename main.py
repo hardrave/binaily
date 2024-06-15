47e51d4117796c12e6d03008cd6f6a886c99f01d
Michal <97395447+hardrave@users.noreply.github.com>
2024-06-15 22:58:08 +0200

    speech.py added

--- main.py
+++ main.py
@@ -1,46 +1,13 @@
-import base64
-import requests
-import os
-import json
+#ImportError: cannot import name 'vision' from 'vision' (/home/mich/binaily/binaily/vision.py)
 
-api_key  = os.getenv("APIKEY")
-
-def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
-
-image_path = "/home/mich/smieci/1.jpg"
-
-base64_image = encode_image(image_path)
-
-headers = {
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {api_key}"
-}
-
-payload = {
-  "model": "gpt-4o",
-  "messages": [
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "You are an ai recycling bin. You gives one word replying, classifying the image as plastic, paper or glass."
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}"
-          }
-        }
-      ]
-    }
-  ],
-  "max_tokens": 300
-}
-
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+from vision import trash
+#from servos import servorun
+#from distance import distanceSensor
 
 
-print(response.json())
+#imagepath = "/home/mich/smieci/1.jpg"
+#vision = trash(imagepath)
+#print(vision)
+
+hiPrompt = open("prompts/hi.txt", "r")
+print(hiPrompt.read())
---
+++ vision.py
@@ -0,0 +1,47 @@
+import base64
+import requests
+import os
+import json
+
+def trash(imagepath):
+
+  image_path = imagepath
+  api_key  = os.getenv("APIKEY")
+
+  def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+      return base64.b64encode(image_file.read()).decode('utf-8')
+
+  
+
+  base64_image = encode_image(image_path)
+
+  headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+  }
+
+  payload = {
+    "model": "gpt-4o",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "You are an ai recycling bin. You gives one word reply, classifying the image as plastic, paper or glass."
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+          }
+        ]
+      }
+    ],
+    "max_tokens": 300
+  }
+
+  response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+  return response.json()
