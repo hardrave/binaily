7a10c7b0d9fb1e17b3370ea6e940f33e61b4065c
Michal <97395447+hardrave@users.noreply.github.com>
2024-06-16 00:56:52 +0200

    voices

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-#ImportError: cannot import name 'vision' from 'vision' (/home/mich/binaily/binaily/vision.py)
-
-from vision import trash
+#from vision import trash
 #from servos import servorun
 #from distance import distanceSensor
-
+import vlc
 
 #imagepath = "/home/mich/smieci/1.jpg"
 #vision = trash(imagepath)
 #print(vision)
 
-hiPrompt = open("prompts/hi.txt", "r")
-print(hiPrompt.read())
+#hiPrompt = open("prompts/hi.txt", "r")
+#print(hiPrompt.read())
+
--- speech.py
+++ speech.py
@@ -7,11 +7,11 @@ api_key = os.getenv("APIKEY")
 # Ensure the api_key is passed when creating the client
 client = OpenAI(api_key=api_key)
 
-speech_file_path = Path(__file__).parent / "speech.mp3"
+speech_file_path = "/home/mich/source/binaily/voice/disscus.mp3"
 response = client.audio.speech.create(
   model="tts-1",
-  voice="alloy",
-  input="Today is a wonderful day to build something people love!"
+  voice="nova",
+  input="Maybe you have any questions about recycling in Warsaw? I can help you!",
 )
 
 response.stream_to_file(speech_file_path)
